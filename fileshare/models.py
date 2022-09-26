3b16aa02cf9918b4515bc5c1579de978d85f5eeb
Partho Debnath <71305747+pd28CSE@users.noreply.github.com>
2022-09-26 18:52:37 +0600

    rename user uploaded file name and then save

--- fileshare/models.py
+++ fileshare/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.timezone import localtime, now
 
 from random import randint
 
@@ -13,9 +14,24 @@ class UserIPAddress(models.Model):
         return str(self.ipaddress)
 
 
+def content_file_name(instance, filename):
+    file_extrention = filename.split('.')[-1]
+    '''
+    instance pk is not created yet, because this instance has not been saved yet. 
+    '''
+    if instance.pk:  
+        filename = '{}.{}'.format(instance.pk, file_extrention)
+    else:
+        local_time = localtime(now())
+        time = str(local_time.time()).split('.')[0].replace(':', '-')
+        filename = '{}--{}.{}'.format(local_time.date(), time, file_extrention)
+    return filename
+
+
+
 class File(models.Model):
     
-    file = models.FileField(upload_to='mediafiles')
+    file = models.FileField(upload_to=content_file_name)
     datetime = models.DateTimeField(default=timezone.now()+timezone.timedelta(days=7))
     id = models.PositiveBigIntegerField(primary_key=True, unique=True)
 
