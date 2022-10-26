cdece985b97d87bd328b793bdbf62fc5074b9ce2
Partho Debnath <71305747+pd28CSE@users.noreply.github.com>
2022-10-27 01:10:08 +0600

    update "serving static file during production or deployment"

--- FileSharing/settings.py
+++ FileSharing/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
+STATICFILES_DIRS = [  # serving static files "development"
     Path.joinpath(BASE_DIR, 'statics')
 ]
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles') # serving static files "deployment"
 
 
-# Media files
+# User upload files (Media files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'medias')
 
